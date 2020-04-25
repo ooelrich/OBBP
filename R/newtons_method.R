6d9d83971622177718d4653364a099696847ee26
Oscar Oelrich <oscar.oelrich@live.com>
2020-04-25 17:06:32 +0200

    Updated Newton's method.

--- R/newtons_method.R
+++ R/newtons_method.R
@@ -12,7 +12,8 @@
 newtons_method <- function(y, k_xx, precision = 0.05) {
 
     f <- rep(0, length(y))
-
+    f_old <- rep(1, length(y))
+    
     while ((t(f-f_old) %*% (f-f_old)) > precision) {
         w <- diag(exp(as.vector(f)))
         ch <- chol(solve(k_xx + diag(rep(0.01, 16))) + w)
