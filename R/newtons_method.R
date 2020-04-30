6c0679ae11438f3635400610617fed0de03b1b68
Oscar Oelrich <oscar.oelrich@live.com>
2020-04-30 14:28:27 +0200

    new dataset

--- R/newtons_method.R
+++ R/newtons_method.R
@@ -13,8 +13,8 @@ newtons_method <- function(y, k_xx, precision = 0.05) {
 
     f <- rep(0, length(y))
     f_old <- rep(1, length(y))
-    
-    while ((t(f-f_old) %*% (f-f_old)) > precision) {
+
+    while ((t(f - f_old) %*% (f - f_old)) > precision) {
         w <- diag(exp(as.vector(f)))
         ch <- chol(solve(k_xx + diag(rep(0.01, 16))) + w)
         f_old <- f
